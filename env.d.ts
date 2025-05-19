639d8df062cd977e9ae15d2999883bbf27d66b39
Catty <ducvu73@gmail.com>
2025-05-19 17:09:49 +0700

    update: add types for env keys

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    FIREFOX_BIN_PATH: string;
+    CHROME_BIN_PATH: string;
+    CHROME_PROFILE_PATH: string;
+    FIREFOX_PROFILE_PATH: string;
+  }
+}
