2babc82135c65bac6c5774262e74f3cff67fcab1
mlemingcapoo <ducvu73@gmail.com>
2025-05-21 21:52:29 +0700

    update: add locale config and basic info

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
   manifest: {
+    name: "__MSG_extName__",
+    description: '__MSG_extDescription__',
+    default_locale: 'en',
     host_permissions: [
       "*://*.google.com/*",
       "*://*.youtube.com/*",
       // "*://*.spotify.com/*",
       // "*://*.apple.com/*",
       "http://localhost/*"
-    ]
-  }
+    ],
+  },
 });
