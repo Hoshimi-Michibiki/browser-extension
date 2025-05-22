308823354a6e05e8f5c870485c998f9278430ebb
mlemingcapoo <ducvu73@gmail.com>
2025-05-22 23:22:26 +0700

    update: simple component for testing

---
+++ components/injected-ui/MusicSiteButton.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useI18n } from '@/composables/useI18n';
+
+const { t, getUILanguage } = useI18n();
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <button class="playerButton" type="button" @click="count++">{{ msg }} : {{ count }}</button>
+</template>
+
+<style scoped>
+.playerButton{
+    border-radius: 15px;
+}
+</style>
