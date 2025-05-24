0c6b0d3b39f6cc104f8459dc9d44bec6a1693652
mlemingcapoo <ducvu73@gmail.com>
2025-05-24 20:46:18 +0700

    add: template for site adapters

---
+++ content-scripts/sites/_common/README.ts
@@ -0,0 +1,63 @@
+import { mountInjectedComponent } from "@/content-scripts/ui-injector";
+import ReadMeButton from "@/components/injected-ui/README.vue";
+import { selectors } from "../selectors";
+import { waitForElementByXPath, waitForElementBySelector, observeDOM } from "@/utils/dom-helpers";
+import { logger } from '@/utils/logger';
+import { ContentScriptContext } from "#imports";
+
+// import css framework here because 
+//! important: always add '?inline' at the end of the file name
+import pureCssContent from '@/public/material-you/css/material-design-light.min.css?inline';
+
+export const hostnamePatterns: RegExp[] = [/^(?:www\.)google\.com$/i];
+
+interface InjectedComponents {
+    componentName?: string;
+    unmount: () => void;
+}
+
+let unmountFunctions: InjectedComponents[] = [];
+
+async function injectButtonIntoPlayer(context: ContentScriptContext) {
+    try {
+        const sel = await selectors();
+        if (!sel) {
+            throw new Error(`We can't work without selectors.`);
+        };
+        const playBar = await waitForElementByXPath(sel.google.xpath.searchBarButtonsParent);
+        if (playBar && !playBar.querySelector('.michibiki-injected-button')) {
+            logger.info(`Injecting our cute button...`);
+            const mountPoint = document.createElement('div');
+            mountPoint.className = 'michibiki-injected-button';
+            playBar.appendChild(mountPoint);
+            logger.debug(`playbar: `, playBar);
+            const { unmount, componentName } = await mountInjectedComponent(
+                context, // required
+                ReadMeButton, // vue component
+                mountPoint, // a parent div that the component will be injected in
+                { msg: 'Ext Action'}, // component props
+                'Component name here', // component name (anything you want)
+                pureCssContent // inline CSS for the component, you can add multiple files like this: cssFile1 + cssFile2 + cssFile3 and so on...
+            );
+            unmountFunctions.push({componentName, unmount});
+            // return; // just testing
+            unmountFunctions.forEach(({unmount, componentName})=>{
+                logger.debug(unmount, componentName);
+                // this is to unmount the only needed component
+                // if (componentName === 'Component name here') {
+                //     unmount();
+                // }
+            });
+            logger.warn(`execution stops here..`);
+        } else {
+            logger.info(`Seems like the cute button has already exists or injection has failed`);
+        }
+    } catch (error) {
+        logger.error(`Ouch... error!`, error);
+    }
+}
+
+export const init = (context: ContentScriptContext): void => {
+    logger.info(`Adapter for Google initialized!`);
+    injectButtonIntoPlayer(context);
+};
